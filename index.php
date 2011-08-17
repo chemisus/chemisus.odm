021cc4c2f43482294ccee5f3859cf2b72ac64d2f
Terrence Howard <chemisus@gmail.com>
2011-08-17 18:15:14 -0400

    added orm.php, which includes the files necessary for chemisus.odm

--- index.php
+++ index.php
@@ -40,10 +40,7 @@
     });
 </script>
 <?php
-require_once('connection.php');
-require_once('server.php');
-require_once('database.php');
-require_once('document.php');
+require_once('odm.php');
 
 class Object
 {
---
+++ odm.php
@@ -0,0 +1,5 @@
+<?php
+require_once('connection.php');
+require_once('server.php');
+require_once('database.php');
+require_once('document.php');
