e50d900c0bb5d4d33156e0058774010cf4beb307
Terrence Howard <chemisus@gmail.com>
2014-03-23 15:03:19 -0400

    tested doc creation error

--- test/Snuggie/CreateDocumentTest.php
+++ test/Snuggie/CreateDocumentTest.php
@@ -35,4 +35,30 @@ class CreateDocumentTest extends TestCase
 
         $this->assertEquals($expect, $document->rev);
     }
+
+    public function testCreateDocumentFail()
+    {
+        $rf = new ResponseFactory();
+        $op = new CreateDocument($rf);
+        $connection = Mockery::mock('Snuggie\Connection');
+        $database = 'recipes';
+        $id = 'SpaghettiWithMeatballs';
+        $body = '{"description": "An Italian-American dish that usually consists of spaghetti, tomato sauce and meatballs.","ingredients": ["spaghetti","tomato sauce","meatballs"],"name": "Spaghetti with meatballs"}';
+        $response = 'HTTP/1.1 400 Bad Request' . "\n" .
+            'Cache-Control: must-revalidate' . "\n" .
+            'Content-Length: 85' . "\n" .
+            'Content-Type: application/json' . "\n" .
+            'Date: Wed, 14 Aug 2013 20:31:39 GMT' . "\n" .
+            'ETag: "1-917fa2381192822767f010b95b45325b"' . "\n" .
+            'Location: http://localhost:5984/recipes/SpaghettiWithMeatballs' . "\n" .
+            'Server: CouchDB (Erlang/OTP)' . "\n" .
+            '' . "\n" .
+            '{}';
+
+        $connection->shouldReceive('request')->with('PUT', $database . '/' . $id, $body)->once()->andReturn($response);
+
+        $this->shouldThrow('Snuggie\DocumentCreationException', function () use ($connection, $database, $id, $op, $body) {
+            $op->createDocument($connection, $database, $id, $body);
+        });
+    }
 }
